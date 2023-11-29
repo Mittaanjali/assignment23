282fcc8c1b2fa8aa0a9029ea0bfca6903620b598
Mittaanjali <mitta.anjali@mphasis.com>
2023-11-29 16:07:15 +0530

    assignment23

--- Assignment23/Program.cs
+++ Assignment23/Program.cs
@@ -1,98 +1,71 @@
 ﻿using System;
 
-namespace Assignment23
-{
-    public delegate string WinnerCondition(int energyLevel, int winningProbability);
 
-    internal class Program
+namespace conapprecursionex
+{
+    internal class program
     {
-        public static string CheckWinner(int energyLevel, int winningProbability)
-        {
-            if (energyLevel >= 4 && winningProbability > 60)
-            {
-                return "Congratulations! You are the Winner!";
-            }
-            else if (energyLevel >= 1 && winningProbability > 50)
-            {
-                return "You're the Runner Up!";
-            }
-            else
-            {
-                return "Sorry, Better luck next time. You're a Loser!";
-            }
-        }
+        // Delegate to define the spin function signature
+        delegate void SpinDelegate(ref int energyLevel, ref int winningProbability);
 
         static void Main(string[] args)
         {
-            Console.WriteLine("Enter Your Name:");
-            string playerName = Console.ReadLine();
-
-            Console.WriteLine("Enter Your Lucky Number from 1 to 10:");
-            int luckyNumber = int.Parse(Console.ReadLine());
-
-            int energyLevel = 1;
-            int winningProbability = 100;
-            int noOfSpins = 10;
-
-            WinnerCondition winnerCondition = CheckWinner;
-
-            for (int spin = 1; spin <= noOfSpins; spin++)
+            do
             {
-                Console.WriteLine($"Spin {spin}: Energy Level {energyLevel}, Winning Probability {winningProbability}");
 
-                switch (spin)
+
+                Console.Write("Enter Your Name: ");
+                string playerName = Console.ReadLine();
+
+                int energyLevel = 1;
+                int winningProbability = 100;
+                int noOfSpins = 5;
+
+                // Array of spin functions based on the rules
+                SpinDelegate[] spins =
                 {
-                    case 1:
-                        energyLevel += 1;
-                        winningProbability += 10;
-                        break;
-                    case 2:
-                        energyLevel += 2;
-                        winningProbability += 20;
-                        break;
-                    case 3:
-                        energyLevel -= 3;
-                        winningProbability -= 30;
-                        break;
-                    case 4:
-                        energyLevel += 4;
-                        winningProbability += 40;
-                        break;
-                    case 5:
-                        energyLevel += 5;
-                        winningProbability += 50;
-                        break;
-                    case 6:
-                        energyLevel -= 1;
-                        winningProbability -= 60;
-                        break;
-                    case 7:
-                        energyLevel += 1;
-                        winningProbability += 70;
-                        break;
-                    case 8:
-                        energyLevel -= 2;
-                        winningProbability -= 20;
-                        break;
-                    case 9:
-                        energyLevel -= 3;
-                        winningProbability -= 30;
-                        break;
-                    case 10:
-                        energyLevel += 10;
-                        winningProbability += 100;
-                        break;
-                    default:
-                        break;
-                }
-            }
+             (ref int e, ref int w) => { e += 1; w += 10; },
+             (ref int e, ref int w) => { e += 2; w += 20; },
+             (ref int e, ref int w) => { e -= 3; w -= 30; },
+             (ref int e, ref int w) => { e += 4; w += 40; },
+             (ref int e, ref int w) => { e += 5; w += 50; },
+             (ref int e, ref int w) => { e -= 1; w -= 60; },
+             (ref int e, ref int w) => { e += 1; w += 70; },
+             (ref int e, ref int w) => { e -= 2; w -= 20; },
+             (ref int e, ref int w) => { e -= 3; w -= 30; },
+             (ref int e, ref int w) => { e += 10; w += 100; }
+            };
 
-            string result = winnerCondition(energyLevel, winningProbability);
-            Console.WriteLine("\nWinner: " + result);
+                Console.WriteLine($"Hello {playerName}!");
+                Console.Write("Enter Your Lucky Number from 1 to 10: ");
+                int luckyNumber = int.Parse(Console.ReadLine());
+
+                // Execute spins based on lucky number
+                for (int i = 0; i < noOfSpins; i++)
+                {
+                    Console.WriteLine($"Spin {i + 1}:");
+                    spins[luckyNumber - 1](ref energyLevel, ref winningProbability);
+                    Console.WriteLine($"Energy Level: {energyLevel}, Winning Probability: {winningProbability}");
+
+                    if (energyLevel >= 4 && winningProbability > 60)
+                    {
+                        Console.WriteLine("Winner!");
+                    }
+                    else if (energyLevel >= 1 && winningProbability > 50)
+                    {
+                        Console.WriteLine("Runner Up!");
+                    }
+                    else
+                    {
+                        Console.WriteLine("Loser!");
+                    }
+                }
+                Console.WriteLine("Press any key to play again or 'Q' to quit.");
+            } while (Console.ReadKey().Key != ConsoleKey.Q);
+
 
             Console.ReadKey();
         }
     }
 }
-
 
